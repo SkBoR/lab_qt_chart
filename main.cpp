ab79ac1f27cb221d5ddf00f3a7823649507b5ec4
tan4ek <ssr205@yandex.ru>
2016-12-10 02:15:06 +0300

    Работает отображение из файла, парсинг, сделать по человечески

--- main.cpp
+++ main.cpp
@@ -6,36 +6,19 @@
 #include <QtCharts/QValueAxis>
 #include <QtCharts/QLogValueAxis>
 
+#include <QRegularExpression>
+#include <QDebug>
+#include <QFile>
+#include <QStringRef>
+#include <QList>
+#include <QStringList>
+
 using namespace QtCharts;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QLineSeries *series = new QLineSeries();
-    *series << QPointF(1, 1) << QPointF(2, 73) << QPointF(3, 268) << QPointF(4, 17) << QPointF(5, 4325) << QPointF(6, 723);
-
-    QChart *chart = new QChart();
-    chart->addSeries(series);
-    chart->legend()->hide();
-    chart->setTitle("Logarithmic axis example");
-
-    QValueAxis *axisX = new QValueAxis;
-    axisX->setTitleText("Data point");
-    axisX->setTickCount(6);
-    axisX->setLabelFormat("%i");
-    chart->addAxis(axisX, Qt::AlignBottom);
-    series->attachAxis(axisX);
-
-    QLogValueAxis *axisY = new QLogValueAxis;
-    axisY->setLabelFormat("%g");
-    axisY->setTitleText("Values");
-    axisY->setBase(8);
-    chart->addAxis(axisY, Qt::AlignLeft);
-    series->attachAxis(axisY);
-
-    QChartView *chartView = new QChartView(chart);
-    chartView->setRenderHint(QPainter::Antialiasing);
 
     QBrush prush;
     prush.setStyle(Qt::SolidPattern);
@@ -44,6 +27,86 @@ int main(int argc, char *argv[])
     transform.scale(2.0,2.0);
     prush.setTransform(transform);
 
+    QRegExp regexp("(?=\"time\")(?>.*\n){7}(?=\"time\")");
+
+
+    QRegularExpression regular("(?=\"time\")(?>.*\n){7}", QRegularExpression::MultilineOption);
+
+    QFile file("/home/ssr/Programming/Qt_project/ChartLab/input/inv.tr0");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)){
+        //TODO уведомление о не прочитанном файле
+        return 1;
+    }
+
+    QTextStream in(&file);
+    QString result = in.readAll();
+
+
+    QStringRef *toParse;
+    int indexValues = result.indexOf("VALUE");
+    if(indexValues > -1){
+        toParse = new QStringRef(&result, indexValues, result.length() - indexValues);
+    } else {
+        //TODO сделать красиво
+        return 1;
+    }
+
+    QRegularExpressionMatchIterator match = regular.globalMatch(*toParse);
+
+
+    QLineSeries *series = new QLineSeries();
+//        *series << QPointF(1, 1) << QPointF(2, 73) << QPointF(3, 268) << QPointF(4, 17) << QPointF(5, 4325) << QPointF(6, 723);
+
+
+    //TODO сделать уведомление о том что, что-то не распарсилось
+    while(match.hasNext()){
+        QRegularExpressionMatch localMatch = match.next();
+        if(localMatch.hasMatch()){
+            QString captured = localMatch.captured(0);
+
+            QStringList list = captured.split("\n");
+
+            QString timeValue = list.at(0).split(" ").at(1);
+            double time = timeValue.toDouble();
+
+            // TODO сделать для множества точек (не хардкод кол-во)
+            QString values = list.at(3);
+            double value = values.toDouble();
+//            *series << QPointF()
+                       series->append((float)time, (float)value);
+            qDebug() << "time: " << time;
+            qDebug() << "value: " << value;
+            qDebug() << captured;
+        }
+    }
+
+    qDebug() << series->points();
+
+
+
+
+    QChart *chart = new QChart();
+    chart->addSeries(series);
+    chart->legend()->hide();
+    chart->setTitle("Logarithmic axis example");
+
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+
+    QValueAxis *axisX = new QValueAxis;
+    axisX->setLabelFormat("%f");
+    axisX->setTitleText("Data point");
+    axisX->setTickCount(6);
+    chart->addAxis(axisX, Qt::AlignBottom);
+    series->attachAxis(axisX);
+
+    QValueAxis *axisY = new QValueAxis;
+    axisY->setLabelFormat("%f");
+    axisY->setTitleText("Values");
+    axisY->setTickCount(6);
+    //    axisY->setBase(8);
+    chart->addAxis(axisY, Qt::AlignLeft);
+    series->attachAxis(axisY);
 
     chart->setPlotAreaBackgroundBrush(prush);
     chart->setPlotAreaBackgroundVisible(true);
@@ -52,8 +115,6 @@ int main(int argc, char *argv[])
     window.setCentralWidget(chartView);
     window.resize(400, 300);
     window.show();
-
-    chartView->hide();
 
     //    MainWindow w;
 
